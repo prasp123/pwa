247ac9066507125d61cdd45920a29c0e10090081
prashanth K <46801805+prasp123@users.noreply.github.com>
2023-02-22 23:37:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,17 @@
-self.addEventListener('push',()=>{
-    self.registration.sendNotification('hello world',{});
-})
+self.addEventListener('push', function (event) {
+    console.log('Push received with this data: "${event.data.text()}"');
+    const title = 'Peggy says:';
+    let options = {
+        body: event.data.text(),
+        icon: 'images/peggy_parrot.jpg',
+        actions: [
+            {
+                action: "feed", title: "Feed Peggy"
+            },
+            {
+                action: "wait", title: "Wait to Feed Peggy"
+            }
+        ]
+    };
+    event.waitUntil(self.registration.showNotification(title, options));
+});
